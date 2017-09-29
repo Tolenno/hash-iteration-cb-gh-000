29e169e3e2e6f14db5cdf2684985ef8e35c7eaa9
Matt Hough <matt@hough.tv>
2017-09-29 12:09:56 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@
 def select_winner(passengers)
   winner = ""
   passengers.each do |key, value|
-    winner = value if key == :suit_a && value.start_with("A")
+    if key == :suit_a && value.start_with("A")
   end
   winner
 end
